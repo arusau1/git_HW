d4b1ea68890327cd6ec9f6a25394df14d6a01e94
Artsiom <artsiomrusau@rambler.ru>
2021-06-20 19:47:50 +0300

    delete comments 3

--- src/test/java/guru/qa/SearchTests.java
+++ src/test/java/guru/qa/SearchTests.java
@@ -23,8 +23,6 @@ public class SearchTests {
         Configuration.browser = "chrome";
     }
 
-    //comment1
-
     @BeforeEach
     void openGooglePage() {
         logger.info("BeforeEach Method");
